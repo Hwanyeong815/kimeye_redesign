191c71449ef4364678571648531760733f9fb539
Hwanyeong815 <viggitore@gmail.com>
2025-11-12 22:47:15 +0900

    1112 js경로수정

--- js/main.js
+++ js/main.js
@@ -34,35 +34,35 @@ const youtubeListSlide = () => {
     const youtubeList = [
         {
             id: 1,
-            imgurl: '/images/yotube_thum_0.jpg',
+            imgurl: '../images/yotube_thum_0.jpg',
             date: '2025.07.09',
             videoTitle: '성형안과 의사가 알려주는 쌍커풀 재수술 Q&A',
             videoLink: 'https://www.youtube.com/embed/vhGe2aGIf44',
         },
         {
             id: 2,
-            imgurl: '/images/yotube_thum_1.jpg',
+            imgurl: '../images/yotube_thum_1.jpg',
             date: '2025.07.10',
             videoTitle: '스마트폰을 달고 사는 나한테 딱 맞는 백내장 인공수정체는?',
             videoLink: 'https://www.youtube.com/embed/hYJTQG99QTI',
         },
         {
             id: 3,
-            imgurl: '/images/yotube_thum_2.jpg',
+            imgurl: '../images/yotube_thum_2.jpg',
             date: '2025.07.11',
             videoTitle: '렌즈삽입술에 대한 모든 것! 라식, 라섹과 다른 점은?',
             videoLink: 'https://www.youtube.com/embed/w6zb_YNfCDo',
         },
         {
             id: 4,
-            imgurl: '/images/yotube_thum_3.jpg',
+            imgurl: '../images/yotube_thum_3.jpg',
             date: '2025.07.12',
             videoTitle: '황반변성 주사치료 받기 전후 유의사항',
             videoLink: 'https://www.youtube.com/embed/1Hv7lLi2g50',
         },
         {
             id: 5,
-            imgurl: '/images/yotube_thum_4.jpg',
+            imgurl: '../images/yotube_thum_4.jpg',
             date: '2025.07.13',
             videoTitle:
                 '재준이는 모르는 알록달록한 세상, 과연 나는 볼 수 있을까? 집에서 해보는 색약 테스트!',
